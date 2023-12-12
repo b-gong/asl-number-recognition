b5145e3d5b0e485936a8fde95556361a57580551
blgong <blgong@usc.edu>
2023-12-11 23:32:25 -0800

    center cropped data

---
+++ baseline/generate_data_2.py
@@ -0,0 +1,71 @@
+import os
+import random
+import shutil
+from PIL import Image
+
+# Path to your dataset directory
+source_path = '../assets'
+dataset_path = '../dataset'
+
+# Proportions for train, dev, and test
+TRAIN_PROPORTION = 0.7
+DEV_PROPORTION = 0.2
+TEST_PROPORTION = 0.1
+
+# Creating directories for train, dev, and test sets
+train_dir = os.path.join(dataset_path, 'train_crop')
+dev_dir = os.path.join(dataset_path, 'dev_crop')
+test_dir = os.path.join(dataset_path, 'test_crop')
+
+os.makedirs(train_dir, exist_ok=True)
+os.makedirs(dev_dir, exist_ok=True)
+os.makedirs(test_dir, exist_ok=True)
+
+def center_crop(img, new_w, new_h):
+    w, h = img.size
+    left = (w-new_w)/2
+    top = (h-new_h)/2
+    right = (w+new_w)/2
+    bottom = (h+new_h)/2
+    return img.crop((left, top, right, bottom))
+
+# Function to split files into train, dev, and test sets
+def split_files(source_folder, train_dest, dev_dest, test_dest, size=(64, 64)):
+    files = [f for f in os.listdir(source_folder) if os.path.isfile(os.path.join(source_folder, f))]
+    random.shuffle(files)
+
+    train_count = int(len(files) * TRAIN_PROPORTION)
+    dev_count = int(len(files) * DEV_PROPORTION)
+
+    for i, file in enumerate(files):
+        source = os.path.join(source_folder, file)
+
+        # Open the image
+        img = Image.open(source)
+        # Resize to 64x64
+        img = center_crop(img, 64, 64)
+        # Convert to grayscale (if needed)
+        img = img.convert("L")
+
+        if i < train_count:
+            img.save(os.path.join(train_dest, file))
+        elif i < train_count + dev_count:
+            img.save(os.path.join(dev_dest, file))
+        else:
+            img.save(os.path.join(test_dest, file))
+
+# Splitting images for each label
+for folder in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'unknown']:
+    source_folder = os.path.join(source_path, folder)
+
+    train_dest = os.path.join(train_dir, folder)
+    dev_dest = os.path.join(dev_dir, folder)
+    test_dest = os.path.join(test_dir, folder)
+
+    os.makedirs(train_dest, exist_ok=True)
+    os.makedirs(dev_dest, exist_ok=True)
+    os.makedirs(test_dest, exist_ok=True)
+
+    split_files(source_folder, train_dest, dev_dest, test_dest)
+
+print("Splitting complete!")
