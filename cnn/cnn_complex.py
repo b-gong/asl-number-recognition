19ef942763e22da629cee6fbd12adcdd72576155
unknown <ethanyhsoo@gmail.com>
2023-12-12 18:20:48 -0800

    added F1 metric

--- cnn/cnn_complex.py
+++ cnn/cnn_complex.py
@@ -206,7 +206,39 @@ class ASLCNN_V5(nn.Module):
         x = self.softmax(x)
 
         return x
+    
 
+class ASLCNN_V6(nn.Module):
+    def __init__(self):
+        super(ASLCNN_V6, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5, padding=2)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=5, padding=2)
+
+        self.batchnorm1 = nn.BatchNorm2d(32)
+        self.batchnorm2 = nn.BatchNorm2d(64)
+        self.batchnorm3 = nn.BatchNorm2d(128)
+
+        self.pool = nn.AvgPool2d(2)
+        self.dropout1 = nn.Dropout(0.35)
+
+        self.fc1 = nn.Linear(128 * 8 * 8, 512)
+        self.dropout2 = nn.Dropout(0.6)
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        x = F.relu(self.batchnorm1(self.conv1(x)))
+        x = self.pool(x)
+        x = F.relu(self.batchnorm2(self.conv2(x)))
+        x = self.pool(x)
+        x = F.relu(self.batchnorm3(self.conv3(x)))
+        x = self.pool(x)
+
+        x = x.view(-1, 128 * 8 * 8)
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        return x
 
 # Training function
 def train(model, X_train, y_train, X_dev, y_dev, lr=1e-1, batch_size=32, num_epochs=10, momentum=0):
@@ -242,15 +274,16 @@ def train(model, X_train, y_train, X_dev, y_dev, lr=1e-1, batch_size=32, num_epo
     model.load_state_dict(best_checkpoint)
     print(f'Best Epoch: {best_epoch}, Best Dev Accuracy: {best_dev_acc:.4f}')
 
-def train_NLLL(model, X_train, y_train, X_dev, y_dev, lr=1e-1, batch_size=32, num_epochs=10, momentum=0):
+    # Training function
+def train_F1(model, X_train, y_train, X_dev, y_dev, lr=1e-1, batch_size=32, num_epochs=10, momentum=0):
     model.train()
-    loss_func = nn.NLLLoss()
+    loss_func = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
 
     train_data = TensorDataset(X_train, y_train)
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
-    best_dev_acc = -1
+    best_dev_f1 =-1
     best_checkpoint = None
     best_epoch = -1
 
@@ -260,21 +293,20 @@ def train_NLLL(model, X_train, y_train, X_dev, y_dev, lr=1e-1, batch_size=32, nu
             logits = model(x_batch)
             loss = loss_func(logits, y_batch)
             loss.backward()
-            optimizer.step()
+            optimizer.step()    
+        train_acc, train_prec, train_rec, train_f1 = evaluate_F1(model, X_train, y_train, 'Train')
+        dev_acc, dev_prec, dev_rec, dev_f1 = evaluate_F1(model, X_dev, y_dev, 'Dev')
 
-        train_acc = evaluate(model, X_train, y_train, 'Train')
-        dev_acc = evaluate(model, X_dev, y_dev, 'Dev')
-
-        if dev_acc > best_dev_acc:
-            best_dev_acc = dev_acc
+        if dev_f1 > best_dev_f1:
+            best_dev_f1 = dev_f1
             best_checkpoint = copy.deepcopy(model.state_dict())
             best_epoch = epoch
 
-        print(f'Epoch {epoch}: Train Accuracy: {train_acc:.4f}, Dev Accuracy: {dev_acc:.4f}')
+        print(f'Epoch {epoch}: Train F1: {train_f1:.4f}, Dev F1: {dev_f1:.4f}')
 
     model.load_state_dict(best_checkpoint)
-    print(f'Best Epoch: {best_epoch}, Best Dev Accuracy: {best_dev_acc:.4f}')
-
+    print(f'Best Epoch: {best_epoch}, Best Dev F1: {best_dev_f1:.4f}')
+    
 
 # Evaluation function
 def evaluate(model, X, y, name):
@@ -285,6 +317,41 @@ def evaluate(model, X, y, name):
         acc = torch.mean((y_preds == y).float()).item()
     print(f'{name} Accuracy: {acc:.5f}')
     return acc
+
+# Evaluation with F1
+def evaluate_F1(model, X, y, name):
+    model.eval()
+    with torch.no_grad():
+        logits = model(X)
+        y_preds = torch.argmax(logits, dim=1)
+        
+        #t = (y_preds == y)
+
+        # macro averages
+        precision = 0
+        recall = 0
+        f1 = 0
+        for i in range(10):
+            #p = (y_preds == i)
+            #tp = torch.sum(torch.logical_and(t, p).int())
+            #fp = torch.sum(torch.logical_and(~t, p).int())
+            #fn = torch.sum(torch.logical_and(~t, ~p).int())
+            tp = torch.sum(((y_preds == i) & (y == i)).to(torch.uint8))
+            fp = torch.sum(((y_preds == i) & (y != i)).to(torch.uint8))
+            fn = torch.sum(((y_preds != i) & (y == i)).to(torch.uint8))
+            prec = tp / (tp+fp)
+            precision += prec
+            rec = tp/(tp+fn)
+            recall += rec
+            f1 += 2/((1/prec) + (1/rec))
+            
+        precision /= 10
+        recall /= 10
+        f1 /= 10
+
+        acc = torch.mean((y_preds == y).float()).item()
+
+    return (acc, precision, recall, f1)
 
 def main():
     # Parse command line arguments
@@ -299,7 +366,7 @@ def main():
     torch.manual_seed(0)
 
     # Load the data
-    all_data = np.load('asl_data.npy', allow_pickle=True).item()
+    all_data = np.load('asl_data_crop.npy', allow_pickle=True).item()
     X_train = torch.tensor(all_data['X_train'], dtype=torch.float)
     y_train = torch.tensor(all_data['y_train'], dtype=torch.long)
     X_dev = torch.tensor(all_data['X_dev'], dtype=torch.float)
@@ -313,19 +380,25 @@ def main():
     X_test = X_test.view(-1, 1, 64, 64)
 
     # Create the ASLCNN model
-    model = ASLCNN_V5()
+    model = ASLCNN_V6()
 
     # Train the model
     print("Training model...")
-    train(model, X_train, y_train, X_dev, y_dev, lr=args.learning_rate, 
+    train_F1(model, X_train, y_train, X_dev, y_dev, lr=args.learning_rate, 
           batch_size=args.batch_size, num_epochs=args.num_epochs, momentum=args.momentum)
 
     # Evaluate the model
     print("\nEvaluating model...")
-    train_acc = evaluate(model, X_train, y_train, 'Train')
-    dev_acc = evaluate(model, X_dev, y_dev, 'Dev')
-    test_acc = evaluate(model, X_test, y_test, 'Test')
+    train_acc, train_prec, train_rec, train_f1 = evaluate_F1(model, X_train, y_train, 'Train')
+    dev_acc, dev_prec, dev_rec, dev_f1 = evaluate_F1(model, X_dev, y_dev, 'Dev')
+    test_acc, test_prec, test_rec, test_f1 = evaluate_F1(model, X_test, y_test, 'Test')
+    
+    print(f'Train Acc: {train_acc:.4f}, Dev Acc: {dev_acc:.4f}, Test Acc: {test_acc:.4f}')
+    print(f'Train Prec: {train_prec:.4f}, Dev Prec: {dev_prec:.4f}, Test Prec: {test_prec:.4f}')
+    print(f'Train Rec: {train_rec:.4f}, Dev Rec: {dev_rec:.4f}, Test Rec: {test_rec:.4f}')
+    print(f'Train F1: {train_f1:.4f}, Dev F1: {dev_f1:.4f}, Test F1: {test_f1:.4f}')
 
+    print ()
     print(f'\nFinal Performance: Train Acc: {train_acc:.5f}, Dev Acc: {dev_acc:.5f}, Test Acc: {test_acc:.5f}')
 
 if __name__ == '__main__':
